41e0db8835fd64db894c3dd2903dcd8e1611c180
luizcarvalhosilva <156116231+luizcarvalhosilva@users.noreply.github.com>
2025-07-09 13:26:26 -0300

    style(java): substitui println por print para melhorar a exibição do programa
    Ajusta a forma como os dados são exibidos no console, tornando a saída mais legível e contínua.

--- WhatYourName.java
+++ WhatYourName.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class WhatYourName {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Olá, informe seu nome");
+        System.out.print("Olá, informe seu nome ");
         String userName = scanner.next();
-        System.out.println("Qual sua idade?");
+        System.out.print("Qual sua idade? ");
         int userAge = scanner.nextInt();
         System.out.printf("Olá, %s. Você tem %d anos.", userName, userAge);
     }
