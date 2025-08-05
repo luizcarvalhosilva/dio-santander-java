6db7caacf4c61a72af10f318dcdaa3b10f5ef1c2
luizcarvalhosilva <156116231+luizcarvalhosilva@users.noreply.github.com>
2025-08-05 16:33:40 -0300

    feat(conta-banco): implementação da lógica para solicitar dados e exibir mensagens ao usuário
    Adicionada a lógica para capturar dados do usuário (como número da conta, agência e nome do cliente) e exibir mensagens personalizadas com base nas informações fornecidas.

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Cliente, informe seu nome: ");
+        String clientName = scan.next();
+
+        System.out.println("Informe o número da conta: ");
+        int account = scan.nextInt();
+
+        System.out.println("Por favor, digite o número da Agência!");
+        String branch = scan.next();
+
+        System.out.println("Informe o seu saldo: ");
+        double balance = scan.nextDouble();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, ", clientName);
+        System.out.printf("sua agência é %s, conta %d e seu saldo %.2f ", branch, account, balance);
+        System.out.println("já está disponível para saque.");
+        
+        scan.close();
     }
 }
