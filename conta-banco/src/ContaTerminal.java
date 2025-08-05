d8a46cde1fec0ce56773697047e99aef69fdb070
luizcarvalhosilva <156116231+luizcarvalhosilva@users.noreply.github.com>
2025-08-05 16:47:45 -0300

    style(conta-banco): melhora indentação e ajusta entrada do nome do cliente para usar nextLine()
    Código reorganizado para melhor legibilidade e corrigida a captura do nome do cliente, permitindo entradas com espaços ao utilizar nextLine().

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -6,7 +6,7 @@ public class ContaTerminal {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Cliente, informe seu nome: ");
-        String clientName = scan.next();
+        String clientName = scan.nextLine();
 
         System.out.println("Informe o número da conta: ");
         int account = scan.nextInt();
@@ -17,10 +17,10 @@ public class ContaTerminal {
         System.out.println("Informe o seu saldo: ");
         double balance = scan.nextDouble();
 
-        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, ", clientName);
-        System.out.printf("sua agência é %s, conta %d e seu saldo %.2f ", branch, account, balance);
-        System.out.println("já está disponível para saque.");
-        
+        System.out.printf(
+                "Olá %s, obrigado por criar uma conta em nosso banco. Sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.%n",
+                clientName, branch, account, balance
+        );
         scan.close();
     }
 }
